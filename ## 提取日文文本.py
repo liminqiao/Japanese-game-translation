43cdfeae448630103d9860d5a5a35010742e9f1b
liminqiao <119308065+liminqiao@users.noreply.github.com>
2025-08-25 23:12:15 +0800

    提取日本文本

---
+++ ## 提取日文文本.py
@@ -0,0 +1,44 @@
+import re
+import os
+# 读取文件内容并逐行处理
+file_path = r'd:\blgame_and_tool\game\弄り遊戯（リマスター版）\GameMaterial\itazura_dsm_decrypt\２．辰巳.txt'
+out_path = r'd:\blgame_and_tool\game\弄り遊び（リマスター版）\extracted_japanese.txt'
+
+# 确保输出文件夹存在
+out_dir = os.path.dirname(out_path)
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+
+pattern1 = r'[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF、。！？「」【】［］（）～]+'  # 判断是否含日文字符
+pattern2 = r'[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFFＡ-Ｚａ-ｚA-Za-z0-9、。！？「」『』【】［］（）～:…―]+'  # 提取日文文本
+jump_pattern = r'\[jump.*\]'  # 匹配包含[jump ...]的行
+with open(file_path, 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+# 提取日文文本
+japanese_lines = []
+
+for line in lines:
+    if re.search(jump_pattern, line):
+        continue
+    # 替换[r]为冒号
+    
+    line = line.replace("[r]", ":")
+    
+    # 检查是否包含日文字符（使用pattern1）
+    if re.search(pattern1, line):
+        # 提取日文文本（使用pattern2）
+        extracted_text = re.findall(pattern2, line)
+        if extracted_text:
+            # 将匹配的部分合并为一个字符串
+            combined_text = ''.join(extracted_text)
+            japanese_lines.append(combined_text)
+            print(combined_text)
+
+# 将提取的日文文本写入文件
+with open(out_path, 'w', encoding='utf-8') as out_file:
+    out_file.writelines([line + '\n' for line in japanese_lines])
+
+print('a')
+
